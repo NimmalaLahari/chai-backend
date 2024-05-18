f6ab087895c68a3cff945efac308eece39d6c9a4
NimmalaLahari <lakshmilavanyalahari23@gmail.com>
2024-05-18 12:31:48 +0530

    all setu configured

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
+
 const app=express()
 //express configurations
 app.use(cors(
@@ -9,7 +10,7 @@ app.use(cors(
         credentials:true 
     }
 ))
-
+//middlewares
 app.use(express.json({limit: "16kb"}))//for json data
 app.use(express.urlencoded({extended: true,limit:"16kb"}))//extract data from url
 app.use(express.static("public"))//configuring public assets
@@ -19,9 +20,17 @@ app.use(cookieParser())//
 //     console.log(`App is listening on port ${process.env.PORT}`)
 // })
 
-app.on("error",(error)=>{
+//routes import
+
+import userRouter from './routes/user.routes.js'
+
+//routes declaration(using middleware;as logic and route are written
+    //in a separate files--so middlewares connect them)
+app.use("/api/v1/users",userRouter)//a prefix route
+
+/*app.on("error",(error)=>{
     console.log("ERR: ",error)
     throw error
-})
+})*/
 
 export{ app }
